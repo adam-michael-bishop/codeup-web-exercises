eb56d851504b499a8fdfaee13ea5447fb873ce57
Adam Bishop <adam.michael.bishop@gmail.com>
2022-11-22 14:58:58 -0600

    Added the files for Functions Exercies. Added code for the TODO comments in functions.js

--- js/external.js
+++ js/external.js
@@ -1,6 +1,6 @@
 "use strict";
 
-console.log("Hello from external JavaScript")
+console.log("Hello from external JavaScript");
 
 alert('Welcome to my Website!');
 
@@ -9,16 +9,13 @@ let userFavColor = prompt('What is your favorite color?');
 alert(`Neat, ${userFavColor.toLowerCase()} is my favorite color too!`);
 
 let littleMermaidDays = Number(prompt('How many days did you rent "The Little Mermaid" for?'));
-
 let brotherBearDays = Number(prompt('How many days did you rent "Brother Bear" for?'));
-
 let herculesDays = Number(prompt('How many days did you rent "Hercules" for?'));
-
 let pricePerDay = Number(prompt('What was the price per day for rental?'));
-
 let totalPrice = (littleMermaidDays + brotherBearDays + herculesDays) * pricePerDay;
 
 alert(`Your total price is ${totalPrice}`);
+
 
 
 
