937de7d33ca1962640be6c3213c756d7855eb97a
Adam Bishop <adam.michael.bishop@gmail.com>
2022-11-29 09:16:59 -0600

    Added and completed files for conditionals exercise. Added shuffleArray function in cards.js

--- blackjack/cards.js
+++ blackjack/cards.js
@@ -22,6 +22,7 @@ function assignSuit(cardRank){
 function shuffleArray(arr){
     for (let i = arr.length - 1; i > 0; i--){
         let j = Math.floor(Math.random() * (i + 1));
+        console.log(i, j);
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 }
