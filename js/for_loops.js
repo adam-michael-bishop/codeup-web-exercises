7df07f2f56bcd5cd4e9a1b24a8b5b14793a28139
Adam Bishop <adam.michael.bishop@gmail.com>
2022-12-01 10:53:57 -0600

    Completed exercises for for_loops.js

---
+++ js/for_loops.js
@@ -0,0 +1,47 @@
+(() => {
+    'use strict';
+
+    function showMultiplicationTable(number) {
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${number} x ${i} = ${number * i}`);
+        }
+    }
+
+    function generateTenRandomNumbers(minNumber, maxNumber) {
+
+        for (let i = 0; i < 10; i++) {
+            let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber)
+            if (randomNumber % 2 === 0) {
+                console.log(`${randomNumber} is even`);
+                continue;
+            }
+            console.log(`${randomNumber} is odd`);
+        }
+    }
+
+    function numberPyramid(length) {
+
+        for (let i = 1; i <= length; i++) {
+
+            let numberString = '';
+            for (let j = i; j > 0; j--) {
+                numberString += `${i}`;
+            }
+            console.log(numberString);
+        }
+    }
+
+    function decrementByFive(number) {
+        for (let i = number; i > 0; i -= 5) {
+
+            console.log(i);
+
+        }
+    }
+
+    showMultiplicationTable(7);
+    generateTenRandomNumbers(20, 200);
+    numberPyramid(9);
+    decrementByFive(100);
+})();
+
