ddf270e4cc11013b020c2517cae0e7747b9fb1dc
Adam Bishop <adam.michael.bishop@gmail.com>
2022-12-01 13:12:11 -0600

    Added prompts to displayMainMenu in main.js

--- blackjack/js/main.js
+++ blackjack/js/main.js
@@ -4,10 +4,10 @@ let playingHand = false;
 let player = {hand: [], score: 0};
 let dealer = {hand: [], score: 0};
 
-function initGame() {
+function initGame(){
     playingHand = false;
-    player = {hand: [], score: 0, total: 0};
-    dealer = {hand: [], score: 0, total: 0};
+    player = {hand: [], score: 0};
+    dealer = {hand: [], score: 0};
 }
 
 function printScores(){
@@ -15,8 +15,23 @@ function printScores(){
 }
 
 function displayMainMenu(){
+    console.clear();
     printScores();
-    console.log('Main Menu');
+    console.log('Main Menu\n \n \nWaiting for player prompt...');
+    let userInput = prompt("Enter 1 or start to play Blackjack, enter 2 to exit game...");
+    if(userInput === null){
+        displayMainMenu();
+    } else if(userInput.trim() === "1" || userInput.trim().toLowerCase() === "start"){
+        initGame()
+        playingHand = true;
+    } else if(userInput.trim() === "2" || userInput.trim().toLowerCase() === "exit") {
+        let playerConfirmQuit = confirm("Are you sure you want to quit?");
+        if(playerConfirmQuit){
+            window.close();
+        } else {
+            displayMainMenu();
+        }
+    }
 }
 
 initGame();
