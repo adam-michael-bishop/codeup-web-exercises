8819e0d7a68ea5805e9d93e340a5474d7f2f85b2
Adam Bishop <adam.michael.bishop@gmail.com>
2022-12-02 10:42:49 -0600

    added TODO comments to playGame function.

--- blackjack/js/main.js
+++ blackjack/js/main.js
@@ -6,6 +6,7 @@ let dealer = {hand: [], score: 0};
 
 function initGame(){
     resetScores();
+    resetHands();
     if(playingHand){
         playGame();
     } else {
@@ -14,8 +15,13 @@ function initGame(){
 }
 
 function resetScores(){
-    player = {hand: [], score: 0};
-    dealer = {hand: [], score: 0};
+    player.score = 0;
+    dealer.score = 0;
+}
+
+function resetHands(){
+    player.hand = [];
+    player.score = [];
 }
 
 function printScores(){
@@ -46,8 +52,31 @@ function playGame(){
     resetScores();
     while (playingHand) {
         printScores();
+        /** TODO:
+         *  deal 1 card to the player face up
+         *  deal 1 card to the dealer face down
+         *  1 card to player face up
+         *  1 to dealer face up
+         *  ---------
+         *  check for player blackjack
+         *  if player has blackjack then check for dealer blackjack otherwise player wins the hand
+         */
         let playerGameMenuInput = prompt('Choose to Hit, Stand, or Return to Main Menu\n1: Hit\n2: Stand\n3: Main Menu');
 
+        if (playerGameMenuInput === "1") {
+            /** TODO:
+             *  If player chose hit, deal them 1 card face up
+             *  Add the total of the player's hand and display that to player
+             *  Check if player busts, if so they lose the hand and dealer gets +1 to score
+             *
+             */
+        }
+        if (playerGameMenuInput === "2") {
+            /** TODO:
+             *
+             *
+             */
+        }
         if(playerGameMenuInput === "3") {
             let playerConfirmQuit = confirm("Are you sure you want to quit?");
             if(playerConfirmQuit) {
